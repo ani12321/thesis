8c97c69655633ffe5481d40a68ecaf5dcd72f962
ani12321 <ani_xhaja@hotmail.com>
2018-01-25 23:40:10 +0100

    initial commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+
+app.use(serveStatic(path.join(__dirname, 'public-optimized')))
+app.use(serveStatic(path.join(__dirname, '../public')))
+
+app = app.listen(process.env.PORT || 8888, process.env.IP || "0.0.0.0", function() {
+    var addr = app.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
+});
+
+require('./WebRTC-Scalable-Broadcast.js')(app);
