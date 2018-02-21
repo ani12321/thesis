9027a731f6c6ec2d5dd8574c96e155101e188fc4
ani12321 <ani_xhaja@hotmail.com>
2018-02-22 00:42:30 +0100

    shortest path algorithm

--- src/graph.js
+++ src/graph.js
@@ -25,9 +25,11 @@ let Alg = require("graphlib").alg;
 class GraphData {
 
     constructor(broadcast) {
+        this.shortestPath = this.shortestPath.bind(this);
+        this.add = this.add.bind(this);
         if (!broadcast) return;
         this.broadcast = broadcast;
-        this.graph = new Graph();
+        this.graph = new Graph({directed: false});
         this.graph.setNode(this.broadcast.broadcaster.userid);
     }
 
@@ -43,13 +45,8 @@ class GraphData {
             this.graph.setEdge(this.broadcast.broadcaster.userid, clients[client].userid, {
                 weight: 10 // TODO: calculate the weights
             });
-
-            this.graph.setEdge(clients[client].userid, this.broadcast.broadcaster.userid, {
-                weight: 10 // TODO: calculate the weights
-            });
         }
 
-        console.log(this.graph.edges());
     }
 
     add(userId, broadcasterId) {
@@ -57,11 +54,6 @@ class GraphData {
         this.graph.setEdge(broadcasterId, userId, {
             weight: 10
         });
-
-        this.graph.setEdge(userId, broadcasterId, {
-            weight: 10
-        });
-        console.log(this.graph.edges());
     }
 
     join(client) {
@@ -69,9 +61,24 @@ class GraphData {
     }
 
     shortestPath(userId) {
-        console.log(this.graph.edges());
+        if(this.graph.edges().length == 0) return this.broadcast.broadcaster;
 
-        return Alg.dijkstra(this.graph, userId.toString(), (e) => this.graph.edge(e))
+        let distances = Alg.dijkstra(this.graph, userId, (e) => this.graph.edge(e).weight)
+        console.log(this.graph.edges())
+        let paths = [];
+        for(var key in distances) {
+            if(distances[key].distance == 0) continue;
+
+            paths.push({
+                distance: distances[key].distance,
+                node: key
+            })
+        }
+
+        console.log(paths)
+        paths.sort((a, b) => a.distance > b.distance)
+        let selectedNode = paths[0].node;
+        return this.broadcast.clients[selectedNode];
     }
 
 }
--- src/stream-algorithm.js
+++ src/stream-algorithm.js
@@ -1,15 +1,11 @@
 module.exports = function(broadcast, userToJoin) {
     // TODO: extend the algorithm
-    // console.log(broadcast)
+    let streamHost = broadcast.graph.shortestPath(userToJoin.userid)
+    // at this moment the userToJoin is not in the graph
+    // and therefore we cannot find his shortest path.
+    // need to find a solution for this.
 
-
-    let streamHost = broadcast.broadcaster;
-    const clients = Object.keys(broadcast.clients);
-    if(clients.length > 1) streamHost = broadcast.clients[clients[0]];
-
-    console.log(userToJoin);
-    broadcast.graph.add(userToJoin.id, streamHost.id)
-    // console.log(broadcast.graph.shortestPath(userToJoin.id))
+    broadcast.graph.add(userToJoin.userid, streamHost.userid)
 
     return streamHost;
 }
